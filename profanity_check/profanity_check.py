dce4f2d06b1782892f94e1cf5c78c426b1260ce1
Menelaos Kotoglou <menelaoskotoglou@gmail.com>
2020-06-29 17:10:25 +0300

    Update profanity_check.py

--- profanity_check/profanity_check.py
+++ profanity_check/profanity_check.py
@@ -9,10 +9,7 @@ def _get_profane_prob(prob):
   return prob[1]
 
 def predict(texts):
-  if model.predict_proba(vectorizer.transform(texts)) >= 0.75:
-    return 1
-  else:
-    return 0
+  return model.predict(vectorizer.transform(texts))
 
 def predict_prob(texts):
   return np.apply_along_axis(_get_profane_prob, 1, model.predict_proba(vectorizer.transform(texts)))
